b74668b3b302e5d8e860c76ed0ecc9d3a6616372
Bradley <bradley@griffindork.com>
2023-06-08 20:40:21 -0700

    I dunno what I'm doing

---
+++ scripts/fetch_and_reset.sh
@@ -0,0 +1,7 @@
+git fetch --quiet >/dev/null
+git clean --quiet -df >/dev/null
+git reset --quiet --hard "@{u}" >/dev/null
+git checkout --quiet main >/dev/null
+git clean --quiet -df >/dev/null
+git reset --quiet --hard "@{u}" >/dev/null
+git submodule update --init --recursive
